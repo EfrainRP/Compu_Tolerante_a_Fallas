fd001228d2ac401f393ff51fe2aa620c04df534f
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2023-10-28 16:50:34 -0600

    Update Dockers

--- Dockers/index.js
+++ Dockers/index.js
@@ -1,24 +1,28 @@
-import express from 'express'
-import mongoose from 'mongoose'
+//Importa los modulos dependencias necesarios para este codigo, sitio web simple
+import express from 'express' //Dependencia de NodeJs
+import mongoose from 'mongoose' //Dependencia para MongoDB
 
-const Animal = mongoose.model('Animal', new mongoose.Schema({
+const Animal = mongoose.model('Animal', new mongoose.Schema({ //Crea un modelo de una base de datos ANIMAL en MongoDB
   tipo: String,
   estado: String,
 }))
 
-const app = express()
+const app = express()// Crea una instancia para tener una aplicacion web principal 
 
-mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://efra:password@mongodb:27017/miapp?authSource=admin') //Se especifica la conexion a la DB(base de datos) de Mongo de acuerdo al docker
+//El cual se le asigna un usuario y contraseña, despues se conectara al contenedor de la BD a usar
 
-app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
-  const animales = await Animal.find();
-  return res.send(animales)
+//Primera ruta raiz
+app.get('/', async (_req, res) => {//Del metodo GET, empezara a listar los animales en la base de datos de mongo
+  console.log('listando... animales...')//Imprimira en consola el mensaje
+  const animales = await Animal.find();//Obtienen todos los animales en la BD
+  return res.send(animales)//Regresara la lista de los animales encontrados
 })
-app.get('/crear', async (_req, res) => {
-  console.log('creando...')
-  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
-  return res.send('ok')
+//Otra ruta
+app.get('/crear', async (_req, res) => {//Del metodo SET, empezara a agregar los animales a la base de datos de mongo
+  console.log('creando...')//Imprimira en consola el mensaje
+  await Animal.create({ tipo: 'Animal', estado: 'Feliz' })//Agrega valor a la BD
+  return res.send('ok')//Regresara un mensaje de ok, de animal guardado
 })
 
-app.listen(3000, () => console.log('listening...'))
+app.listen(3000, () => console.log('listening...'))//mensaje que estara escuchando la WEB, en el puerto 3000
