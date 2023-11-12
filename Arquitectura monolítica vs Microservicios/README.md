e235d9c1da1fff5cce48fc89b76e165da0471b4a
LAPTOP_Efrain <efrain.robles5009@alumnos.udg.mx>
2023-11-12 15:46:38 -0600

    Update microservices

--- Arquitectura monolítica vs Microservicios/README.md
+++ Arquitectura monolítica vs Microservicios/README.md
@@ -1,7 +1,27 @@
-# Runnng identitical node microservice with nginx loadbalancer
+# Arquitectura monolítica vs Microservicios
 
-1. `git clone git@github.com:bhandariprakash/dockerize-elasticsearch.git`
-2. go to project folder.
-3. Create node app image  `docker build -t node-app .` 
-4. Run `docker-compose up` command
-5. Open `http://localhost:8080/` in browser. This will open app in browser.
+CARRERA: **Ingeniería en Computación**
+
+**Efrain Robles Pulido** CÓDIGO: **221350095**
+
+**Computación Tolerante a Fallas**
+
+MAESTRO: **Michel Emanuel Lopez Franco**
+
+SECCIÓN: **D06**    CALENDARIO: **2023B**
+
+**UNIVERSIDAD DE GUADALAJARA**
+
+![CUCEI Logo](https://static.wixstatic.com/media/689543_e867e5de31ce49e7a2c28f84eb1bacf8~mv2.png/v1/fill/w_560,h_150,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/logoudggris.png)
+
+Para poder realizar esta actividad fue necesario instalar ***Docker Desktop***, para Windows desde la pagina principal de **[Docker]**(https://docs.docker.com/desktop/install/windows-install/).
+Una vez instalado la aplicacion (y verificado que funcione correctamente, iniciando el programa), utilizado en la [practica anterior](link del git anterior).
+
+## Microservicio
+En esta ocasion nuestro microservicio sea relizado en 4 diferentes contenedores para que con la ayuda del balanceador de carga de nginx pueda direccionarnos a cada contenedor, por lo que en nuestro navegador nos mostrara un mensaje con el puerto del contenedor en el que se direcciono. 
+
+Asi que nuestro balanceador de carga de Nginx distribuira el trafica/peticines de los clientes hacia los multiples contenedores.
+
+Si descargas estos archivos de esta carpeta, y ejecutas primero el comando `docker build -t node-app .` y despues el comando `docker compose up`, podras probar por ti mismo la pagina microservicio que se realizo.
+
+## Conclución
--- Arquitectura monolítica vs Microservicios/index.js
+++ Arquitectura monolítica vs Microservicios/index.js
@@ -1,36 +1,9 @@
-import mongoose from 'mongoose' //Dependencia para MongoDB
-
-const Animal = mongoose.model('Animal', new mongoose.Schema({ //Crea un modelo de una base de datos ANIMAL en MongoDB
-    tipo: String,
-    estado: String,
-}))
-
-mongoose.connect('mongodb://efra:password@mongodb:27017/miapp?authSource=admin') //Se especifica la conexion a la DB(base de datos) de Mongo de acuerdo al docker
-//El cual se le asigna un usuario y contraseña, despues se conectara al contenedor de la BD a usar
-
 const express = require('express');
 const app = express()
 const appId = process.env.APPID;
 
-/*app.get('/', (req, res) => {
-    console.log(appId)
-    if (appId == '2222'){
-        return res.send(`HOLA : ${appId}`);
-    }else{
-    return res.send(`Opening app : ${appId}`)
-}})*/
+app.get('/', (req, res) => { res.send(`Opening app : ${appId}`)}) //Ruta raiz de nuestra pagina
 
-//Primera ruta raiz
-app.get('/', async (_req, res) => {//Del metodo GET, empezara a listar los animales en la base de datos de mongo
-    console.log('listando... animales...')//Imprimira en consola el mensaje
-    const animales = await Animal.find();//Obtienen todos los animales en la BD
-    return res.send(animales)//Regresara la lista de los animales encontrados
-})
-//Otra ruta
-app.get('/crear', async (_req, res) => {//Del metodo SET, empezara a agregar los animales a la base de datos de mongo
-    console.log('creando...')//Imprimira en consola el mensaje
-    await Animal.create({ tipo: 'Animal', estado: 'Feliz' })//Agrega valor a la BD
-    return res.send('ok')//Regresara un mensaje de ok, de animal guardado
-})
+app.get('/crear', (req, res) => {res.send(`Opening app : ${appId}      ok`)}) //Ruta secundaria que simulara otro servicio
 
-app.listen(appId, () => console.log(`App listening on port ${appId}!`))
+app.listen(appId, () => console.log(`App listening on port ${appId}!`)) //Mensaje de puertos activos
